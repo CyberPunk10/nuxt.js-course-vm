56f033420187568c056b4c0ee8275c6f400fa803
CyberPunk10 <konstantin21_arx@gmail.com>
2020-11-09 19:58:03 +0300

    Create page Dream

---
+++ layouts/emptyCenter.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="empty-center-layout">
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    error() {
+      return this.$store.getters.error
+    }
+  },
+  watch: {
+    error(value) {
+      this.$message.error(value.response.data.message)
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.empty-center-layout
+  height: 100vh
+  display: flex
+  justify-content: center
+  align-items: center
+  width: 100%
+</style>
