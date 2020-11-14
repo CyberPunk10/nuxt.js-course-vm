61c0d67042cb499916ccb2b2aceea98e1ec7208c
CyberPunk10 <konstantin21_arx@gmail.com>
2020-11-14 16:49:03 +0300

    Part 7, 3/3 (Finished Analytics)

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,10 +1,21 @@
 <template>
   <div>
-    <h1>Admin layout</h1>
+    <h1>Аналитика по постам</h1>
     <span>{{message}}</span>
     <el-button @click.prevent="sayGoodbay">
       Result
     </el-button>
+    <hr>
+    <AnalyticsChart
+      title="Количество просмотров"
+      :labels="views.labels"
+      :data="views.data"
+    />
+    <AnalyticsChart
+      title="Количество комментариев"
+      :labels="comments.labels"
+      :data="comments.data"
+    />
   </div>
 </template>
 
@@ -12,11 +23,18 @@
 export default {
   layout: 'admin',
   middleware: ['admin-auth'],
+
+  async asyncData({store}) {
+    const { views, comments } = await store.dispatch('post/getAnalytics')
+    return { views, comments }
+  },
+
   data() {
     return {
       message: 'message'
     }
   },
+
   methods: {
     async sayGoodbay() {
       const result__ = await this.$api.getDate()
