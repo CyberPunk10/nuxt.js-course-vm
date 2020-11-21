e3e19dc135c87268665584706fa773382eece559
CyberPunk_xiaomi <konstantin21_arx@bk.ru>
2020-11-21 12:10:23 +0300

    Development teleportMenu.vue

--- components/TeleportMenu/index.vue
+++ components/TeleportMenu/index.vue
@@ -3,7 +3,7 @@
     class="teleport-menu-wrap"
     v-on:click="handleClickTeleportMenu"
   >
-    <!-- <div></div> class="underlay" -->
+    <div class="underlay"></div>
     <div class="teleport-menu">
       <ul>
         <li><a href="/">SSR блог</a></li>
@@ -53,16 +53,17 @@ export default {
       console.log(event.target.dataset.tmbtn)
       const teleportMenuWrap = document.querySelector('.teleport-menu-wrap')
       const underlay = teleportMenuWrap.querySelector('.underlay')
-      console.log(underlay)
       if (event.target.className === 'underlay') {
         // event.target.classList.toggle('underlay')
-        underlay.remove()
+        // underlay.remove()
+        teleportMenuWrap.classList.toggle('active')
       } else if (event.target.dataset.tmbtn === 'teleport-menu-btn') {
-        if (underlay) {
-        underlay.remove()
-        } else {
-          teleportMenuWrap.insertAdjacentHTML('afterbegin', '<div class="underlay"></div>')
-        }
+        // if (underlay) {
+        // underlay.remove()
+        // } else {
+        //   teleportMenuWrap.insertAdjacentHTML('afterbegin', '<div class="underlay"></div>')
+        // }
+        teleportMenuWrap.classList.toggle('active')
       }
     }
   }
@@ -70,29 +71,71 @@ export default {
 </script>
 
 <style lang="sass">
-.underlay
-  position: fixed
-  top: 0
-  width: 100%
-  height: 100%
-  opacity: 0.3
-  // opacity: 0
-  background-color: #333
-
-.teleport-menu
+.teleport-menu-wrap
   position: fixed
   z-index: 9999
   top: $height-header
-  left: -21rem
+  left: -21.3rem
+  width: 21.3rem
+  max-height: calc(100vh - #{$height-header} + .4rem)
+  // height: 100%
+  // background-color: red
+  // border-top: 1px solid red
+  // border-right: 1px solid red
+  // border-bottom: 1px solid red
+  border-top-right-radius: 2.3rem
+  border-bottom-right-radius: 2.3rem
+  transition: all .2s linear
+
+  background-image: -webkit-linear-gradient(264.51deg, #FFE580 4.38%, #FF7571 11.51%, #FF7270 25.06%, #EA5DAD 36.04%, #C2A0FD 47.63%, #9867F0 59.03%, #3BF0E4 69.96%, #33CE43 83.74%, #B2F4B6 95.62%)
+  background-image: -moz-linear-gradient(264.51deg, #FFE580 4.38%, #FF7571 11.51%, #FF7270 25.06%, #EA5DAD 36.04%, #C2A0FD 47.63%, #9867F0 59.03%, #3BF0E4 69.96%, #33CE43 83.74%, #B2F4B6 95.62%)
+  background-image: -0-linear-gradient(264.51deg, #FFE580 4.38%, #FF7571 11.51%, #FF7270 25.06%, #EA5DAD 36.04%, #C2A0FD 47.63%, #9867F0 59.03%, #3BF0E4 69.96%, #33CE43 83.74%, #B2F4B6 95.62%)
+  background-image: linear-gradient(264.51deg, #FFE580 4.38%, #FF7571 11.51%, #FF7270 25.06%, #EA5DAD 36.04%, #C2A0FD 47.63%, #9867F0 59.03%, #3BF0E4 69.96%, #33CE43 83.74%, #B2F4B6 95.62%)
+  background-position: 58% 50%
+  background-size: 500%
+
+  -webkit-animation: gradient-shift 30s ease infinite
+  animation: gradient-shift 30s ease infinite
+
+
+@keyframes gradient-shift
+  0%
+    background-position: 58% 50%
+  25%
+    background-position: 100% 0%
+  75%
+    background-position: 10% 50%
+  to
+    background-position: 58% 50%
+
+.underlay
+  position: fixed
+  top: 0
+  left: 0
+  width: 100%
+  height: 100%
+  opacity: 0.3
+  opacity: 0
+  background-color: #333
+  z-index: -1
+.teleport-menu
+  // position: fixed
+  // z-index: 9999
+  // top: $height-header
+  // left: -21rem
   width: 21rem
   max-height: calc(100vh - #{$height-header})
   background-color: rgb(248,250,252)
-  border-top: 1px solid rgb(226, 232, 240)
-  border-right: 1px solid rgb(226, 232, 240)
-  border-bottom: 1px solid rgb(226, 232, 240)
+  border-top: 5px solid transparent
+  border-right: 5px solid transparent
+  border-bottom: 5px solid transparent
   border-top-right-radius: 2rem
   border-bottom-right-radius: 2rem
   transition: all .2s linear
+  margin: .2rem
+  margin-left: 0
+  -webkit-background-clip: text
+  background-clip: border-box
 
   ul
     padding: 1.6rem
@@ -169,15 +212,26 @@ export default {
     display: none
 
 
+// // hide
+// .underlay + .teleport-menu
+//   left: 0
+//   box-shadow: 4px 2px 4px rgba(0,0,0,.101562)
+// // .underlay + .teleport-menu .teleport-menu__button
+// //   box-shadow: none
+// .underlay + .teleport-menu .teleport-menu__svg-list
+//   display: none
+// .underlay + .teleport-menu .teleport-menu__svg-close
+//   display: block
+
 // hide
-.underlay + .teleport-menu
+.teleport-menu-wrap.active
   left: 0
   box-shadow: 4px 2px 4px rgba(0,0,0,.101562)
-// .underlay + .teleport-menu .teleport-menu__button
-//   box-shadow: none
-.underlay + .teleport-menu .teleport-menu__svg-list
+.teleport-menu-wrap.active .underlay
+  opacity: 0.3
+.teleport-menu-wrap.active .teleport-menu .teleport-menu__svg-list
   display: none
-.underlay + .teleport-menu .teleport-menu__svg-close
+.teleport-menu-wrap.active .teleport-menu .teleport-menu__svg-close
   display: block
 
 
