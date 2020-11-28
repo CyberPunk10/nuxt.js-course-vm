e6902b85dc7a4223eaf6b3e578394dec2bf91052
CyberPunk10 <konstantin21_arx@gmail.com>
2020-11-28 19:56:27 +0300

    Add component FSD

---
+++ components/FSD/buttons/Checkbox.vue
@@ -0,0 +1,132 @@
+<template>
+  <!-- mixin checkbox-btn(options)
+  -
+    var title = options.title || "label for checkbox"
+    var isChecked = options.checked || false          // checked: true
+    var id = options.id || false
+    var tag = options.tag || false
+
+    // for checkbox-btn__label
+    var classNameLabel = "checkbox-btn__label"
+    var description = options.description || false
+    classNameLabel += options.description ? " checkbox-btn__label_title" : ""
+
+    // for checkbox-btn__description
+    var classNameDescription = "checkbox-btn__description"
+
+
+  if tag
+    li.checkbox-btn
+      input(type="checkbox", class="checkbox-btn__input", id = id checked = isChecked)
+      label(class = classNameLabel, for = id) !{title}
+      if description
+        p.checkbox-btn__description= description
+  else
+    .checkbox-btn
+      input(type="checkbox", class="checkbox-btn__input", id = id checked = isChecked)
+      label(class = classNameLabel, for = id) !{title}
+      if description
+        p.checkbox-btn__description !{description} -->
+
+  <div class="checkbox-btn">
+    <input type="checkbox" class="checkbox-btn__input" id="test">
+    <label for="test" class="checkbox-btn__label">slot</label>
+  </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="sass" scoped>
+.checkbox-btn
+  max-width: 32rem
+  padding: .3rem 0 .32rem
+
+  &__input
+    position: absolute
+    left: -9999px
+
+    &:checked + .checkbox-btn__label:after
+      opacity: 1
+
+    &:not(:checked) + .checkbox-btn__label:after
+      opacity: 0
+
+    &:checked + .checkbox-btn__label:before
+      border-color: $color-purple
+
+    &:checked + .checkbox-btn__label
+      color: $color-dark-shade-75
+
+
+  &__label
+    color: $color-dark-shade-50
+    position: relative
+    cursor: pointer
+    display: inline-block
+    line-height: 1.8rem
+    padding: .3rem 0 .3rem 3rem
+    user-select: none         // делает текст невыделяемым
+
+
+    &:before
+      content: ""
+      position: absolute
+      left: 0
+      top: .2rem
+      width: 2rem
+      height: 2rem
+      background-color: #ffffff
+      box-sizing: border-box
+      border: 1px solid $color-dark-shade-25
+      border-radius: 4px
+
+    &:after
+      content: ""
+      position: absolute
+      left: .6rem
+      top: .8rem
+      width: .8rem
+      height: .6rem
+      box-sizing: border-box
+      border-radius: 1px
+      border-left: 2px solid $color-purple
+      border-bottom: 2px solid $color-purple
+
+      // -webkit-border-image: $gradient-linear-purple
+      // -moz-border-image: $gradient-linear-purple
+      // -o-border-image: $gradient-linear-purple
+      // -ms-border-image: $gradient-linear-purple
+      // border-image: $gradient-linear-purple
+
+      -webkit-transform: rotate(-45deg)
+      -moz-transform: rotate(-45deg)
+      -o-transform: rotate(-45deg)
+      -ms-transform: rotate(-45deg)
+      transform: rotate(-45deg)
+
+    &_title
+      font-weight: bold
+      color: $color-dark-shade-75
+
+  &__description
+    padding: .3rem 0 .1rem 3rem
+    font-size: 1.2rem
+    color: $color-dark-shade-50
+    user-select: none         // делает текст невыделяемым
+    line-height: 1.4rem
+
+
+
+
+
+
+
+// https://habr.com/ru/post/489476/
+// https://itchief.ru/html-and-css/styling-checkbox-and-radio
+
+</style>
--- pages/challenges/create.vue
+++ pages/challenges/create.vue
@@ -12,6 +12,12 @@
       <el-form-item label="Название">
         <el-input v-model="controls.title"></el-input>
       </el-form-item>
+      <el-form-item label="Описание">
+        <el-input v-model="controls.description"></el-input>
+      </el-form-item>
+      <el-form-item label="Категория">
+        <el-input v-model="controls.category"></el-input>
+      </el-form-item>
       <el-form-item label="Цвет">
         <el-radio-group v-model="controls.radioСolor">
           <el-radio :label="'green'">Зелёный</el-radio>
@@ -23,9 +29,9 @@
         <el-input v-model="controls.count" :disabled="controls.checkbox"></el-input>
         <el-checkbox label="Бессрочный" v-model="controls.checked"></el-checkbox>
       </el-form-item>
-
-      План минимум
-      Суть ch
+      <el-form-item label="Программа минимум">
+        <el-input v-model="controls.planMin"></el-input>
+      </el-form-item>
       <el-form-item label="Date">
         <el-col :span="14">
           <el-date-picker type="date" placeholder="Pick a date" v-model="controls.date" style="width: 100%;"></el-date-picker>
@@ -41,6 +47,11 @@
       </el-form-item>
     </el-form>
 
+    <Btn/>
+    <Checkbox />
+    <AppInput v-model="user.name">Name: </AppInput>
+    <AppInput v-model="user.email" type="email">Email: </AppInput>
+    <AppTextArea v-model="user.text">Text: </AppTextArea>
   </div>
 </template>
 
@@ -52,61 +63,70 @@ export default {
   layout: 'challenges',
 
   data() {
-      return {
-        loading: false,
-        controls: {
-          title: 'Challenge-1',
-          date: null,
-          radioСolor: 'green',
-          count: null,
-          checked: true,
-          checkbox: true
-        },
-        rules: {
-          title: [
-            {required: true, message: 'Добавьте название поста', trigger: 'blur'}
-          ],
-          date: [
-            {required: false, message: 'Поле не должно быть пустым', trigger: 'blur'}
-          ],
-          radioСolor: [
-            {required: true, message: 'Поле не должно быть пустым', trigger: 'blur'}
-          ],
-          count: [
-            {required: true, message: 'Поле не должно быть пустым', trigger: 'blur'}
-          ]
-        }
-      }
-    },
+    return {
+      loading: false,
+      controls: {
+        title: 'Challenge-1',
+        description: null,
+        category: 'sport', // творчество
+        radioСolor: 'green',
+        planMin: null,
+        date: null,
+        count: null,
+        checked: true,
+        checkbox: true
+      },
+      rules: {
+        title: [
+          {required: true, message: 'Добавьте название поста', trigger: 'blur'}
+        ],
+        date: [
+          {required: false, message: 'Поле не должно быть пустым', trigger: 'blur'}
+        ],
+        radioСolor: [
+          {required: true, message: 'Поле не должно быть пустым', trigger: 'blur'}
+        ],
+        count: [
+          {required: true, message: 'Поле не должно быть пустым', trigger: 'blur'}
+        ]
+      },
 
-    methods: {
-      onSubmitCreateChallenge() {
-        this.$refs.form.validate(async valid => {
-          if (valid) {
-            this.loading = true
-            try {
-              const formData = {
-                title: this.controls.title,
-                date: this.controls.date,
-                color: this.controls.radioСolor,
-                count: this.controls.count
-              }
-              console.log(formData)
-              await this.$store.dispatch('challenge/create', formData)
-              this.$message.success('Challenge создан')
-              this.controls.title = ''
-            } catch (error) {
-              console.log(error)
-            } finally {
-              this.loading = false
-            }
-          } else {
-            this.$message.warning('Заполните все поля')
-          }
-        })
+      user: {
+        name: '',
+        email: '',
+        text: ''
       }
     }
+  },
+
+  methods: {
+    onSubmitCreateChallenge() {
+      this.$refs.form.validate(async valid => {
+        if (valid) {
+          this.loading = true
+          try {
+            const formData = {
+              title: this.controls.title,
+              date: this.controls.date,
+              color: this.controls.radioСolor,
+              count: this.controls.count
+            }
+            console.log(formData)
+            await this.$store.dispatch('challenge/create', formData)
+            this.$message.success('Challenge создан')
+            this.controls.title = ''
+          } catch (error) {
+            console.log(error)
+          } finally {
+            this.loading = false
+          }
+        } else {
+          this.$message.warning('Заполните все поля')
+        }
+      })
+    }
   }
+}
 </script>
 
 
@@ -119,4 +139,5 @@ h4
 
 .mb3
   margin-bottom: 3rem
+
 </style>
