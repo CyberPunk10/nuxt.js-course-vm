2b96e1ead9cf34e64593ddf666ef7845123db2f6
CyberPunk_xiaomi <konstantin21_arx@bk.ru>
2021-04-11 16:04:33 +0300

    Add params to .vscode

--- modules/mongodb_setup.js
+++ modules/mongodb_setup.js
@@ -3,13 +3,15 @@ const keys = require('../api/keys')
 
 export default function () {
   this.nuxt.hook('render:setupMiddleware', () => {
-    mongoose.connect(keys.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
+    mongoose
+      .connect(keys.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
 
-    }).then(console.log('MongoDB connected...'))
+      })
+      .then(console.log('MongoDB connected...'))
       .catch(error => console.error('MongoDB disconnected...', error))
   })
 }
