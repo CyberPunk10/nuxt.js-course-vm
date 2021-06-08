795dcd8c75f48a14ae010308d2a5231ad34a241c
CyberPunk_xiaomi <konstantin21_arx@bk.ru>
2021-06-08 16:00:37 +0300

    Setup defualt value (test-drobi)

--- store/delTestDrobi.js
+++ store/delTestDrobi.js
@@ -2,9 +2,8 @@ import Vue from 'vue'
 
 export const state = () => ({
   allFractions: [
-    [10, 21],
-    [11, 21],
-    [12, 21]
+    ['', ''],
+    ['', ''],
   ],
   result: ''
 })
@@ -22,47 +21,13 @@ export const mutations = {
 
     function setValue(arr) {
       Vue.set(state.allFractions[indexFraction], arr, +target.value ? +target.value : null)
-
-      // add/remove class .active --> .btn-add-col
-      // if current input является крайним..
-      // if (player[arr].length - 1 == indexInput) {
-      //   // if input value != 0
-      //   if (+target.value !== 0) {
-      //     addClassActiveBtnAddCol(indexForm)
-      //     // if input value == 0
-      //   } else {
-      //     // предполагаем что все крайние inputs пусты
-      //     let emptyLastInputs = true
-
-      //     // перебор result
-      //     for (let i = 0; i < players.length; i++) {
-      //       if (players[i].result[players[i].result.length - 1]) {
-      //         emptyLastInputs = false
-      //         break
-      //       }
-      //     }
-      //     // перебор result2 (if он есть и if emptyLastInputs по прежнему true)
-      //     if (emptyLastInputs && player.result2) {
-      //       for (let i = 0; i < players.length; i++) {
-      //         if (players[i].result2[players[i].result2.length - 1]) {
-      //           emptyLastInputs = false
-      //           break
-      //         }
-      //       }
-      //     }
-
-      //     if (emptyLastInputs) removeClassActiveBtnAddCol(indexForm)
-
-      //   }
-      // }
     }
 
-    // change resultAll
     changeResultAll(state)
   },
 
   addFraction(state) {
-    state.allFractions.push([1, 10])
+    state.allFractions.push(['', ''])
   }
 }
 
@@ -94,18 +59,7 @@ function changeResultAll(state) {
   state.result = tempArray.reduce((sum, current) => sum + current, 0)
 }
 
-function addClassActiveBtnAddCol(indexForm) {
-  const $btnAddCol = document.getElementById(`index-form-${indexForm}`)
-    .querySelector('.form-sport-main .btns-col .btn-add-col')
-  $btnAddCol.classList.add('active')
-}
-function removeClassActiveBtnAddCol(indexForm) {
-  const $btnAddCol = document.getElementById(`index-form-${indexForm}`)
-    .querySelector('.form-sport-main .btns-col .btn-add-col')
-  $btnAddCol.classList.remove('active')
-}
-
 // Что можно улучшить:
 
-// 1.
+// 1. Допилить удаление дроби
 
