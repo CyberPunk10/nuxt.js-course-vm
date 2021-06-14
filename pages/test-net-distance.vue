72cbe3cae8a80d2b7fa03d3eb1f166d468b14d80
CyberPunk_xiaomi <konstantin21_arx@bk.ru>
2021-06-14 12:40:06 +0300

    Пытка встроить TS код, основанный на инстансе Axios не увенчавшаяся успехом

---
+++ pages/test-net-distance.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="wrap-net-distance">
+    <p>Пустая страница</p>
+  </div>
+</template>
+
+<script>
+export default {
+  head: {
+    title: `test-NetDistance | ${process.env.appName}`
+  },
+
+  layout: 'layout-main-challenges',
+
+  data() {
+    return {
+
+    }
+  },
+
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="sass">
+</style>
+
+
+
+
+
+
+
+
+
+
